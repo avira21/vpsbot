1ad2e042f6247dbe84ee2ee64a9c9d99c8886545
avira21 <galaxysuper2109@gmail.com>
2019-05-05 16:20:15 +0500

    init

---
+++ cmds/serverinfo.js
@@ -0,0 +1,20 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let embed = new Discord.RichEmbed()
+    .setDescription("Информация о сервере")
+    .setColor('#10c7e2')
+    .addField("Название сервера",message.guild.name)
+    .addField("Создание сервера",message.guild.createdAt)
+    .addField("Вы присоеденились к серверу",message.guild.joinedAt)
+    .addField("Кол-Во участников",message.guild.memberCount)
+    .addField("Регион",message.guild.region)
+    .addField("IP:81.176.226.156:27015")
+    .setThumbnail(message.guild.iconURL)
+
+    bot.send(embed);
+
+};
+module.exports.help = {
+    name: "serverinfo"
+};
